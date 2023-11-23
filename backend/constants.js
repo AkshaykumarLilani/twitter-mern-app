bbefbf06a4f204c1a2a99465d7c9e7df6c8d466a
Akshay Lilani <akshaylilani60@gmail.com>
2023-11-23 23:17:22 +0530

    👌 IMPROVE: add .env variables

--- backend/constants.js
+++ backend/constants.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const MONGOOSE_URL = process.env.MONGOOSE_URL;
 const PORT = process.env.PORT || 3000;
-const JSON_TOKEN_SECRET = "mysecrettoken";
+const JSON_TOKEN_SECRET = process.env.JSON_TOKEN_SECRET;
 
 module.exports = {
     MONGOOSE_URL,
